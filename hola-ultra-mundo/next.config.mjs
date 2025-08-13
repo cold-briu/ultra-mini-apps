4a831ff4235125414268921695545068292cbb9c
Sandusky <39354014+cold-briu@users.noreply.github.com>
2025-08-13 14:31:44 -0500

    Update next.config.mjs

--- hola-ultra-mundo/next.config.mjs
+++ hola-ultra-mundo/next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       {
         source: '/.well-known/farcaster.json',
         destination:
-          'https://api.farcaster.xyz/miniapps/hosted-manifest/0198a1aa-02d4-b3a8-6fe4-0318225168f7',
+          'https://api.farcaster.xyz/miniapps/hosted-manifest/0198a4e6-ab68-26f5-35e9-bc1699ec73df',
         permanent: false, // temporary redirect
         statusCode: 307,  // explicitly force 307 instead of default 308
       },
